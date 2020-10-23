e0c010d2390891fc75fe596db64605d45ce8dbf3
ToastCoder <vicky.pcbasic@gmail.com>
2020-10-23 11:36:48 +0530

    Added functionality for giving date as input

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import pickle
 from sklearn.preprocessing import PolynomialFeatures
+from datetime import datetime
 
 # IMPORTING THE DATASET
 data = pd.read_csv("data/covid_data.csv")
@@ -17,6 +18,9 @@ model = pickle.load(open("model/covid_model.pkl", 'rb'))
 poly = PolynomialFeatures(degree = 4)
 
 # PREDICTING VALUES USING TRAINED MODEL
-pred_val = int(input("Enter the day number to be predicted: "))
-res = model.predict(poly.fit_transform([[pred_val]]))
-print("The number of cases in day {} is {}".format(pred_val,int(res)))
+fn_date = str(input("Enter the date to be predicted in the format DD-MM-YYYY: "))
+date1 = datetime(2020,3,1)
+date2 = datetime(int(fn_date[6:10]),int(fn_date[3:5]),int(fn_date[0:2]))
+diff = (date2-date1).days
+res = model.predict(poly.fit_transform([[diff]]))
+print("The number of cases in day {} is {}".format(date2,int(res)))

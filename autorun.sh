3d601dceada61797c829cfb19541c7cb0ecd361b
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-07 14:16:51 +0530

    Added user response for script execution

--- autorun.sh
+++ autorun.sh
@@ -2,13 +2,20 @@
 # Author: Vigneshwar Ravichandar
 # Autorun shell script to ensure requirements,train the model and test the model.
 
-sudo apt install python3 -y
-sudo apt install python3-pip -y 
-pip3 install numpy
-pip3 install pandas
-pip3 install sklearn
-pip3 install pickle
-pip3 install matplotlib
-pip3 install datetime
-python3 train.py
-python3 test.py
+echo "Autorun.sh verifies the requirements and runs the code properly"
+echo "To proceed enter Y, To cancel press N"
+read response
+if (($response == "y" || $response =="Y"))
+then
+    sudo apt install python3 -y
+    sudo apt install python3-pip -y 
+    pip3 install numpy
+    pip3 install pandas
+    pip3 install sklearn
+    pip3 install pickle
+    pip3 install matplotlib
+    pip3 install datetime
+    python3 train.py
+    python3 test.py
+fi
+echo "Thank you for watching!"

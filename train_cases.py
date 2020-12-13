50455791697ab135ca741fbf1d87e2487440f7d1
ToastCoder <vicky.pcbasic@gmail.com>
2020-12-13 11:56:53 +0530

    Initial model training

--- train_cases.py
+++ train_cases.py
@@ -14,6 +14,7 @@ from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
 # DATA PREPROCESSING 
@@ -25,29 +26,28 @@ y = dataset.iloc[:,2].values
 x_train,x_val,y_train,y_val = train_test_split(x,y,test_size = 0.2,random_state = 0)
 
 # RESHAPING THE DATA
-x_train=np.reshape(x_train, (-1,1))
-x_val=np.reshape(x_val, (-1,1))
-y_train=np.reshape(y_train, (-1,1))
-y_val=np.reshape(y_val, (-1,1))
+x_train = np.reshape(x_train, (-1,1))
+x_val = np.reshape(x_val, (-1,1))
+y_train = np.reshape(y_train, (-1,1))
+y_val = np.reshape(y_val, (-1,1))
 
 # SCALING THE DATA
-scaler_x = MinMaxScaler()
-scaler_y = MinMaxScaler()
-xtrain_scale=scaler_x.fit_transform(x_train)
-xval_scale=scaler_x.fit_transform(x_val)
-ytrain_scale=scaler_y.fit_transform(y_train)
-yval_scale=scaler_y.fit_transform(y_val)
+scaler = MinMaxScaler()
+xtrain_scaled = scaler.fit_transform(x_train)
+xval_scaled = scaler.fit_transform(x_val)
+ytrain_scaled = scaler.fit_transform(y_train)
+yval_scaled = scaler.fit_transform(y_val)
 
 # DEFINING NEURAL NETWORK AND ITS LAYERS
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(2, input_dim=1, kernel_initializer='normal', activation='relu'))
-model.add(tf.keras.layers.Dense(472, activation = 'relu'))
-model.add(tf.keras.layers.Dense(1,activation = 'linear'))
+model = Sequential()
+model.add(Dense(2, input_dim=1, kernel_initializer='normal', activation='relu'))
+model.add(Dense(472, activation = 'relu'))
+model.add(Dense(1,activation = 'linear'))
 
 # TRAINING THE MODEL
 model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
-history=model.fit(xtrain_scale, ytrain_scale, epochs=500, batch_size=150, verbose=1, validation_split=0.2)
-predictions = model.predict(xval_scale)
+history=model.fit(xtrain_scaled, ytrain_scaled, epochs=500, batch_size=150, verbose=1, validation_split=0.2)
+predictions = model.predict(xval_scaled)
 
 # PLOTTING THE GRAPH FOR TRAIN-LOSS AND VALIDATION-LOSS
 print(history.history.keys())
@@ -58,3 +58,11 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
+
+# CALCULATING THE ESTIMATED ACCURACY
+ypred_scaled = model.predict(xval_scaled)
+y_pred = scaler.inverse_transform(ypred_scaled)
+print(f"The estimated accuracy of the model is: {round(r2_score(y_val,y_pred)*100,4)}")
+
+# SAVING THE MODEL
+model.save('model/model_cases')

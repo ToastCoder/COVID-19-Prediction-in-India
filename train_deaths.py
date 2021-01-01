338453e955f99cf8349e3cdceb6e7c14846e18df
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-01 20:18:36 +0530

    Some changes

--- train_deaths.py
+++ train_deaths.py
@@ -6,16 +6,20 @@
 
 # TOPICS: Regression, Machine Learning, TensorFlow
 
+# DISABLE TENSORFLOW DEBUG INFORMATION
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
-# DATA PREPROCESSING
+# DATA PREPROCESSING 
 dataset = pd.read_csv('data/covid_data.csv')
 x = dataset.iloc[:,1].values
 y = dataset.iloc[:,3].values
@@ -28,8 +32,8 @@ y = np.reshape(y, (-1,1))
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 # DEFINING THE SCALERS
-scaler_x = MinMaxScaler()
-scaler_y = MinMaxScaler()
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
 
 # TRANSFORMING TO SCALED DATA
 xtrain_scaled = scaler_x.fit_transform(x_train)
@@ -38,7 +42,9 @@ ytrain_scaled = scaler_y.fit_transform(y_train)
 
 # DEFINING NEURAL NETWORK AND ITS LAYERS
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(2, input_dim=1, kernel_initializer='normal', activation='relu'))
+model.add(tf.keras.layers.Dense(2, input_dim = 1, kernel_initializer='normal', activation='relu'))
+model.add(tf.keras.layers.Dense(79, activation = 'relu'))
+model.add(tf.keras.layers.Dense(79, activation = 'relu'))
 model.add(tf.keras.layers.Dense(79, activation = 'relu'))
 model.add(tf.keras.layers.Dense(1,activation = 'linear'))
 
@@ -47,7 +53,6 @@ model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
 history=model.fit(xtrain_scaled, ytrain_scaled, epochs=500, batch_size=150, verbose=1, validation_split=0.2)
 
 # PLOTTING THE GRAPH FOR TRAIN-LOSS AND VALIDATION-LOSS
-print(history.history.keys())
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
@@ -62,6 +67,6 @@ y_pred = scaler_y.inverse_transform(ypred_scaled)
 print(f"The estimated accuracy of the model is: {round(r2_score(y_val,y_pred)*100,4)}")
 
 # SAVING THE MODEL
-PATH = './model/model_deaths'
+PATH = './model/model_cases'
 tf.keras.models.save_model(model,PATH)
 print(f"Successfully stored the trained model at {PATH}")

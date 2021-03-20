0a57df008f550a6c6c1b3336c16dbd90277d417a
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-20 09:10:07 +0530

    Some changes

--- src/test.py
+++ src/test.py
@@ -6,12 +6,6 @@
 
 # TOPICS: Regression, Machine Learning, TensorFlow
 
-# DISABLE TENSORFLOW DEBUG INFORMATION
-import os
-os.system('cd ..')
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-print("TensorFlow Debugging Information is hidden.")
-
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
